c4e2fd710f3041513216cffa2abd457b956f41e0
kallen312 <73615486+kallen312@users.noreply.github.com>
2022-04-06 18:26:33 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Declare a function, named plusTwo  that accepts one parameter, a number.
+//Have the function return the number increased by 2.
+
+function plusTwo(num) {
+  let newNum = num * 2;
+  return newNum;
+}
+
+console.log(plusTwo(18));
